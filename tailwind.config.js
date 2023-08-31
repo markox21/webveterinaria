1f4ea8c87d9da36429484fe42786559dfd431687
markox21 <markouwu321@gmail.com>
2023-08-30 23:04:53 -0500

    versionfinal

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,19 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     colors: {
-      'primary': '#f4f4f4',
-      'grayy': '#808080',
+      primary: "#f4f4f4",
+      grayy: "#808080",
     },
 
-    transition:{
-      'nav': 'transition: opacity 0.3s ease-in-out',
+    transition: {
+      nav: "transition: opacity 0.3s ease-in-out",
     },
 
-    screens:{
-      'xd': '1480px',
+    screens: {
+      xd: "1480px",
       // => @media (min-width: 1480px) { ... }
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
